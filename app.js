53a6745383a653c2599b2efdf9ec45c1f4996f6e
blamex321 <laxman2207@outlook.com>
2023-04-25 16:57:41 +0530

    editied few roots

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ const app = express();
 app.use(express.static('public'));
 
 app.get("/",function(req,res){
-
     res.sendFile(__dirname + "/src/index.html");
 })
 
 app.get("/pricing",function(req,res){
-    res.sendFile(__dirname = "/src/pricing.html");
+    res.sendFile(__dirname + "/src/pricing.html");
 })
 
+app.get("/contacts",function(req,res){
+    res.sendFile(__dirname + "/src/contacts.html")
+})
+
+app.get("/signup",function(req,res){
+    res.sendFile(__dirname + "/src/signup.html");
+})
+app.get("/login",function(req,res){
+    res.sendFile(__dirname + "/src/login.html")
+})
 app.listen(3000| process.env.PORT,function(err){
     if(err){
         console.log(err);
