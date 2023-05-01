594ee1f983dd3b142ca6d7f8bcc1ddc0f321bf15
blamex321 <laxman2207@outlook.com>
2023-05-01 12:31:51 +0530

    added payment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
-
+const stripe = require('stripe')('sk_test_51N2Z2dSA6f1HGrBbMIIlHhyckM5yX2PjxSvZsPPw5rKTrB7hGhGqp5m0kQgg1GDbvuaORa6CKw4P91tBew5GPEUm002ZT9xRPK');
 const app = express();
 
 const loginRoute = require("./routes/login");
@@ -22,6 +22,33 @@ app.get("/pricing",function(req,res){
     res.sendFile(__dirname + "/src/pricing.html");
 })
 
+app.get("/checkout",function(req,res){
+    res.sendFile(__dirname+"/src/checkout.html");
+})
+
+app.get("/fail",function(req,res){
+    res.send("paymment failed")
+})
+
+app.get("/success",function(req,res){
+    res.send("Successfull");
+})
+
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: 'price_1N2ZrFSA6f1HGrBb4j1XYbfl',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/fail',
+    });
+    res.redirect(303, session.url);
+});
 
 
 
--- connections/sqlconnection.js
+++ connections/sqlconnection.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.PASSWORD,
-    port:3306,
-    database: 'testing'
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: process.env.PASSWORD,
+//     port:3306,
+//     database: 'testing'
+// });
 
-connection.connect(function(err){
-    if(err){
-        console.log(err);
-    } else {
-        console.log('Connection Successful');
-    }
-});
+// connection.connect(function(err){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log('Connection Successful');
+//     }
+// });
 
-module.exports = connection;
+// module.exports = connection;
